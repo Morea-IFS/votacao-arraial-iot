93e476f7c150a2c87e5c095e54c3545136f94b7d
Silas Santos <152338397+Silas-Prog@users.noreply.github.com>
2024-06-11 21:43:45 -0300

    :art: new uptade added

--- projetourna-oficial.ino
+++ projetourna-oficial.ino
@@ -1,6 +1,7 @@
 //Bibiotecas.
 #include <ESP8266WiFi.h>
 #include <LiquidCrystal_I2C.h>
+#include <Wire.h>
 #include <Keypad.h>
 #include <ESP8266HTTPClient.h>
 
@@ -9,8 +10,8 @@ const byte LINHAS = 4;
 const byte COLUNAS = 3;
 
 //Dados referentes a conexão wifi e servidor.
-const char* ssid = "IFSLIVRE";
-const char* password = "";
+const char* ssid = "MINASTELECOM-SIMONE";
+const char* password = "helena2228";
 const char* serverAddress = "morea-ifs.org";
 
 //Matriz do teclado.
@@ -34,121 +35,131 @@ byte colunasPinos[COLUNAS] = {14, 12, 13};
 Keypad teclado = Keypad(makeKeymap(teclas), linhasPinos, colunasPinos, LINHAS, COLUNAS);
 
 //String vazia onde será armazenada a matricula
-char matricula[12] = "";
-bool matricula_ok = false;
+char matricula[11] = "";
 
 //String vazia onde será armazenado o numero do candidato
-char candidato[4] = "";
-bool candidato_ok = false;
-
+char candidato[3] = "";
 
 //inicio do void
 void setup(){
+
   //comunicação serial iniciada
   Serial.begin(9600);  
   //comunicação wifi iniciada
   WiFi.begin(ssid,password);
-  while(WiFi.status() != WL_CONNECTED){
-    Serial.println("");
-    Serial.println(".");
-   
-  }
-  Serial.println(WiFi.localIP());
   //comunicação lcd i2c iniciada
   lcd.init();
   lcd.backlight();
-  lcd.setCursor(1,0);
-  lcd.print("INFORME A SUA");
-  lcd.setCursor(3,1);
-  lcd.print("MATRICULA");
+  void pedidoMatricula();
+
 }
 void loop(){
 
   char tecla = teclado.getKey();
+  if(tecla && strlen(matricula)<6){
+    void matriculaDigit();
+  }
+  else if(tecla == '#' && strlen(candidato)<2 && strlen(matricula)>=6){
+    void candidatoDigit();
+  }
+  else if(tecla == '#' && strlen(candidato)>=1){
+    void enviarDados();
+  }
+}
 
+void pedidoMatricula(){
+  char tecla = teclado.getKey();
+  lcd.setCursor(1,0);
+  lcd.print("INFORME A SUA");
+  lcd.setCursor(3,1);
+  lcd.print("MATRICULA");
   if(tecla){
-    if(strlen(matricula)<1){    
+    void matriculaDigit();
+  }
+}
+
+void matriculaDigit(){
+  char tecla = teclado.getKey();
+  if(tecla){
+    if(strlen(matricula) < 5){
       if(tecla == '*'){      
         if(strlen(matricula) > 0){
-          matricula[strlen(matricula)-1] = '\0';
+              matricula[strlen(matricula)-1] = '\0';
         }  
-      }else if(tecla == '#'){
-        matricula_ok = true;
-        candidato_ok = false;
-               
-       }else if (strlen(matricula) < 10){          
-          matricula[strlen(matricula)] = tecla;      
-          matricula[strlen(matricula)+1] = '\0';
-        }     
-        lcd.clear();     
-        lcd.setCursor(3,0);
-        lcd.print("MATRICULA:");
-        lcd.setCursor(3,1);
-        lcd.print(matricula);
-        Serial.print("MATRICULA:");
-        Serial.println(matricula);
-        delay(100);
+      }else if (strlen(matricula) < 10){          
+              matricula[strlen(matricula)] = tecla;      
+              matricula[strlen(matricula)+1] = '\0';
+              Serial.print("MATR-CONCAT:");
+              Serial.println(matricula);
+              Serial.print("");
+      }     
+      lcd.clear();     
+      lcd.setCursor(3,0);
+      lcd.print("MATRICULA:");
+      lcd.setCursor(3,1);
+      lcd.print(matricula);
+      Serial.print("MATRICULA:");
+      Serial.println(matricula);
+      delay(100);
     }
-    if(strlen(matricula)<0){
-      if(tecla == '*'){      
-        if(strlen(candidato) > 0){
-          candidato[strlen(candidato)-1] = '\0';
-        }  
-      }else if(tecla == '#' && strlen(candidato)>1){
-        candidato_ok = true;
-        matricula_ok = true;       
-       }else if (strlen(candidato) < 10){          
+  }
+
+}
+void condidatoDigit(){
+  char tecla = teclado.getKey();
+  if(tecla=='#'){
+    if(strlen(matricula)>5){
+      if(strlen(candidato)<2){
+          if(tecla == '*'){      
+            if(strlen(candidato) > 0){
+              candidato[strlen(candidato)-1] = '\0';
+          }  
+        }else if (strlen(candidato) < 10){          
           if(tecla != '#'){
             candidato[strlen(candidato)] = tecla;      
             candidato[strlen(candidato)+1] = '\0';
+            Serial.print("CAND-CONCAT:");
+            Serial.println(candidato);
+            Serial.print("");
           }
-        }
-      lcd.clear();     
-      lcd.setCursor(3,0);
-      lcd.print("CANDIDATO:");
-      lcd.setCursor(3,1);
-      lcd.print(candidato);
-      Serial.print("CANDIDATO:");
-      Serial.println(candidato);
-      delay(100);
-    }     
-        
-        
-    if(candidato_ok == true && matricula_ok == true){
-       lcd.clear();
-       lcd.setCursor(3,0);
-       lcd.print("ESTAMOS ENVIANDO");
-       lcd.setCursor(3,1);
-       lcd.print("AGUARDE");
-        
-       Serial.print("ESTAMOS ENVIANDO!");
-       if (WiFi.status() == WL_CONNECTED) {       
-         HTTPClient http;
-         WiFiClient client;
-         String url = "http://" + String(serverAddress) + "/enviarDados?matricula=" + matricula + "&candidato=" + candidato;                   
-         http.begin(client, url);
-         int httpResponseCode = http.GET();      
-         if (httpResponseCode > 0) {
-           Serial.print("Resposta do servidor: ");
-           Serial.println(http.getString());
-         }else {
-           Serial.print("Falha na solicitação HTTP, código de erro: ");
-           Serial.println(httpResponseCode);
-         }      
-         http.end();
-           memset(matricula, 0, sizeof(matricula));
-           memset(candidato, 0, sizeof(candidato));
-           matricula_ok = false;
-           candidato_ok = false;
-    
-           lcd.clear();
-           lcd.setCursor(1,0);
-           lcd.print("INFORME A SUA");
-           lcd.setCursor(3,1);
-           lcd.print("MATRICULA");
-            
-       }
+        }     
+        lcd.clear();     
+        lcd.setCursor(3,0);
+        lcd.print("CANDIDATO:");
+        lcd.setCursor(3,1);
+        lcd.print(candidato);
+        Serial.print("CANDIDATO:");
+        Serial.println(candidato);
+        delay(100);
+      }
     }
   }
-  delay(100);
+
+}
+void enviarDados(){
+  Serial.print("ESTAMOS ENVIANDO!");
+  int freeHeap = ESP.getFreeHeap();
+  Serial.print("Memória RAM livre: ");
+  Serial.print(freeHeap);
+  Serial.println(" bytes");
+
+  if (WiFi.status() == WL_CONNECTED) {       
+    HTTPClient http;
+    WiFiClient client;
+    String url = "http://" + String(serverAddress) + "/enviarDados?matricula=" + matricula + "&candidato=" + candidato;                   
+    http.begin(client, url);
+    Serial.print("URLSERVER-HTTP: ");
+    Serial.println(url);
+    Serial.println("");
+    int httpResponseCode = http.GET();      
+    if (httpResponseCode > 0){
+      Serial.print("Resposta do servidor: ");
+      Serial.println(http.getString());
+    }
+    else{
+      Serial.print("Falha na solicitação HTTP, código de erro: ");
+      Serial.println(httpResponseCode);
+    }      
+      http.end();     
+  }
 }
